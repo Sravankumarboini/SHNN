445c268d08361cee4589880ee96dd7cbea4a8030
Sravankumarboini <sravankumarboini005@gmail.com>
2026-04-08 14:42:57 +0530

    debug

--- app.py
+++ app.py
@@ -206,8 +206,5 @@ def predict_api():
 # -------------------------------
 # Run Server
 # -------------------------------
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000, debug=True)
